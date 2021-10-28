1070cf3591a1c5406dad73d0c5d00b71ff2f26ad
=Alex <=>
2021-10-28 12:06:56 +0600

    add codewars

---
+++ 01.js
@@ -0,0 +1,19 @@
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    let count = 0;
+    for (let i=0; i< arrayOfSheep.length; i++){
+      if (arrayOfSheep[i] !== false ){
+        count++;
+      }
+    }
+    return count;
+  }
+
+  var array1 = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ];
+
+  console.log(countSheeps(array1))
